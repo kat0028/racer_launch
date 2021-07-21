b13c51761a906109c2393590a578c89ba1ca634c
Kyle Thompson <kat0028@auburn.edu>
2021-07-21 09:03:31 -0500

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/kyle/soft_sys/jet_racer_ws/src/racer_launch
+# Build directory: /home/kyle/soft_sys/jet_racer_ws/src/racer_launch/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/kyle/soft_sys/jet_racer_ws/src/racer_launch/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/kyle/soft_sys/jet_racer_ws/src'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/kyle/soft_sys/jet_racer_ws/src/racer_launch/build/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/kyle/soft_sys/jet_racer_ws/src/racer_launch:$ROS_PACKAGE_PATH"
